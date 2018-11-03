b136af276d20424e6a2624b6533fdfc187810a1a
Rajesh Paudel <rajeshdpaudel@gmail.com>
2018-11-03 21:39:32 +0545

    Improved styling added actions and implementing sockets room

--- index.js
+++ index.js
@@ -5,55 +5,111 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname+'/page/index.html');
 });
-var users =0;
+
 var usersDetail = [];
+var totalUser = 0;
 var usersIp = [];
 var testIp = [];
+var roomsDetail = [];
 
 io.on('connection', function(socket){
-	
+	var currentRoom = '';
+	// console.log(socket);
+
+	// For figuring out the ip address of user
 	usersIp.push(socket.handshake.headers.host);
 	testIp.push(socket.handshake.headers["x-real-ip"]);
 	
+	// console.log("usersIp:" + usersIp);
+	// console.log("testIp:" + testIp);
+	
+	socket.on('createRoom',function(room){
+		roomsDetail.push(room);
+		socket.join(room.name);
+		currentRoom = room.name;
+	});
+
+	socket.on('action',function(action){
+		io.emit('userAction',action);
+	});
+
+
+	socket.on('joinRoom',function(roomCredentials){
+		// Verify room credentials with above roomDetail
+		var requestedRoom = [];
+		
+		for (var i = 0; i < roomsDetail.length; i++) {
+			if(roomsDetail[i].name == roomCredentials.name)
+			{
+				requestedRoom.push(roomsDetail[i]);
+			}
+		}
+
+		if (requestedRoom.length) {
+			if (requestedRoom.passKey == '' || requestedRoom.passKey == roomCredentials.passKey) {
+				if (currentRoom != '') {
+					socket.leave(currentRoom);
+				}
+				socket.join(room.name);
+			}
+		}else{
+			socket.emit('errorJoiningRoom',"Cannot join room");
+		}
+	});
+
 	socket.on('newUser',function(user){
 		socket.username = user;
+		usersDetail.push(user);
 
 		socket.broadcast.emit('newUserConnected', user+" has connected to chat");
-		usersDetail.push(user);
-		io.emit('totalUsers',usersDetail);
+		io.emit('totalUsers',usersDetail.length);
+		
 		console.log("Users : "+usersDetail);
+		console.log('a user connected');
 	});
 	
-	console.log("usersIp:" + usersIp);
-	console.log("testIp:" + testIp);
 	
-	console.log('a user connected');
 
-	users++;
+	totalUser++;
+	
 	// Counting Users
-	console.log("clients: " + users);
+	console.log("clients: " + usersDetail.length);
 
 
 	socket.on('disconnect', function(user){
-		users--;
+		totalUser--;
+		
 		console.log('user disconnected '+ socket.username);
+		
 		usersDetail.pop(socket.username);
 		io.emit('totalUsers',usersDetail);
+		
 
 		if (socket.username != undefined) {
-			io.emit('userDisconnected', socket.username+ " has left the chat");
+			if (currentRoom = '') {
+				io.emit('userDisconnected', socket.username+ " has left the chat");
+			}else{
+				io.to(currentRoom).emit('userDisconnected', socket.username+ " has left the chat");
+			}
 		}
 	});
 
 	// Emitted event on chat form submittion
 	socket.on('chat message', function(msg,user){
-		socket.broadcast.emit('chat message', msg , user);
+		if (currentRoom == "") {
+			socket.broadcast.emit('chat message', msg , user);		
+		}else{
+			socket.broadcast.to(currentRoom).emit('chat message', msg , user);
+		}
 	});
 });
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
+
+
