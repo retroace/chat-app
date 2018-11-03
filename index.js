a720b7de2829f6dc95e8d9fc0998d4689c278824
Rajesh Paudel <rajeshdpaudel@gmail.com>
2018-11-03 16:46:52 +0545

    Fixes for showing currently connected users added user actions

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
@@ -10,8 +9,27 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname+'/page/index.html');
 });
 var users =0;
+var usersDetail = [];
+var usersIp = [];
+var testIp = [];
+
 io.on('connection', function(socket){
-	socket.broadcast.emit('hi');
+	
+	usersIp.push(socket.handshake.headers.host);
+	testIp.push(socket.handshake.headers["x-real-ip"]);
+	
+	socket.on('newUser',function(user){
+		socket.username = user;
+
+		socket.broadcast.emit('newUserConnected', user+" has connected to chat");
+		usersDetail.push(user);
+		io.emit('totalUsers',usersDetail);
+		console.log("Users : "+usersDetail);
+	});
+	
+	console.log("usersIp:" + usersIp);
+	console.log("testIp:" + testIp);
+	
 	console.log('a user connected');
 
 	users++;
@@ -19,15 +37,20 @@ io.on('connection', function(socket){
 	console.log("clients: " + users);
 
 
-	socket.on('disconnect', function(){
+	socket.on('disconnect', function(user){
 		users--;
-		console.log('user disconnected');
+		console.log('user disconnected '+ socket.username);
+		usersDetail.pop(socket.username);
+		io.emit('totalUsers',usersDetail);
+
+		if (socket.username != undefined) {
+			io.emit('userDisconnected', socket.username+ " has left the chat");
+		}
 	});
 
 	// Emitted event on chat form submittion
 	socket.on('chat message', function(msg,user){
 		socket.broadcast.emit('chat message', msg , user);
-
 	});
 });
 
